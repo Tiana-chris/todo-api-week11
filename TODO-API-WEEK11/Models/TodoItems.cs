ed57f3bf0c35c2078a195811f7e77e2e2debd773
Tiana_Chris <amakachukwu215@gmail.com>
2026-05-08 18:38:01 +0100

    Added TodoItem model

--- TODO-API-WEEK11/Models/TodoItems.cs
+++ TODO-API-WEEK11/Models/TodoItems.cs
@@ -2,5 +2,12 @@
 {
     public class TodoItems
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DueDate { get; set; }
+        public string Priority { get; set; } //Low, Medium, High
     }
 }
