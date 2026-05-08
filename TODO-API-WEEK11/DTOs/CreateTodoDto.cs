c892df9c67918a0d666576119d42ad714e65af59
Tiana_Chris <amakachukwu215@gmail.com>
2026-05-08 18:51:17 +0100

    Create DTOs with validations

--- TODO-API-WEEK11/DTOs/CreateTodoDto.cs
+++ TODO-API-WEEK11/DTOs/CreateTodoDto.cs
@@ -1,6 +1,18 @@
-﻿namespace TODO_API_WEEK11.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+namespace TODO_API_WEEK11.DTOs
 {
     public class CreateTodoDto
     {
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public DateTime? DueDate { get; set; }
+        [RegularExpression("Low|Medium|High")]
+        public string Priority { get; set; } = "Medium";
+
     }
 }
